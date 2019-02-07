43aadc6a48435b55d7cf64bc1a1518fc15022c01
hyunjo <hyunjog@gmail.com>
2019-02-07 16:58:32 +0900

    adapter

---
+++ java Design Patterns/src/adapter_01/AdapterImpl.java
@@ -0,0 +1,15 @@
+package adapter_01;
+
+public class AdapterImpl implements Adapter {
+
+	@Override
+	public Float twiceOf(Float f) {
+		return (float) Math.twoTime(f.doubleValue());
+	}
+
+	@Override
+	public Float halfOf(Float f) {
+		return (float) Math.half(f.doubleValue());
+	}
+
+}
